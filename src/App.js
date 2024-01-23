f162052dad34656286b409cf883fc77c8713cc1b
David-F-98 <u2903149@unimilitar.edu.co>
2024-01-23 14:11:50 -0500

    Acciones de Formulario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import FormularioTareas from './Components/FormularioTareas';
 import Header from './Components/Header';
 
 const App = () => {
+  
+  const [tareas, cambiarTareas] = useState(
+    [
+      {
+        id: 1,
+        texto: 'Lavar la ropa',
+        completada: false
+      },
+      {
+        id: 2,
+        texto: 'Comprar arroz',
+        completada: false
+      }
+    ]
+  );
+
   return (
     <div className='contenedor'>
-      <Header></Header>
+      <Header/>
+      <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas}/>
     </div>
   );
 }
